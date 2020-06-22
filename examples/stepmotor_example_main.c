b5e8fad89dfbd4327160e13d2b3456a79d9ecc24
thanhphong98 <thanhphongho1998@gmail.com>
2020-06-22 21:58:36 +0700

    initial commit

---
+++ examples/stepmotor_example_main.c
@@ -0,0 +1,90 @@
+// MIT License
+
+// Copyright (c) 2020 thanhphong98
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "stm_err.h"
+#include "stm_log.h"
+
+#include "stepmotor.h"
+
+/* 
+ * This example shows how to control step motor driver .
+ *      - Pin direction: PD13.
+ *      - Pin pulse: PD12.
+ */
+
+#define TASK_SIZE   1024
+#define TASK_PRIOR  5
+
+#define DIR_GPIO_PORT           GPIO_PORT_D
+#define DIR_GPIO_NUM            GPIO_NUM_13
+#define PULSE_TIMER_NUM         TIMER_NUM_4
+#define PULSE_TIMER_PINS_PACK   TIMER_PINS_PACK_2
+#define PULSE_TIMER_CHANNEL     TIMER_CHNL_1
+
+/* Handle structure */
+stepmotor_handle_t stepmotor_handle;
+
+static const char *TAG = "APP_MAIN";
+
+static void example_task(void* arg)
+{
+    /* Configure step motor */
+    stepmotor_config_t stepmotor_cfg;
+    stepmotor_cfg.dir_gpio_port = DIR_GPIO_PORT;
+    stepmotor_cfg.dir_gpio_num = DIR_GPIO_NUM;
+    stepmotor_cfg.pulse_timer_num = PULSE_TIMER_NUM;
+    stepmotor_cfg.pulse_timer_pins_pack = PULSE_TIMER_PINS_PACK;
+    stepmotor_cfg.pulse_timer_chnl = PULSE_TIMER_CHANNEL;
+    stepmotor_handle = stepmotor_config(&stepmotor_cfg);
+
+    /* Set step driver parameter */
+    stepmotor_set_pwm_duty(stepmotor_handle, 50);
+    stepmotor_set_pwm_freq(stepmotor_handle, 800);
+    stepmotor_set_dir(stepmotor_handle, 0);
+
+    /* Start motor */
+    stepmotor_start(stepmotor_handle);
+
+    while(1)
+    {
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        stepmotor_stop(stepmotor_handle);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        stepmotor_start(stepmotor_handle);
+    }
+}
+
+int main(void)
+{
+    /* Set log output level */
+    stm_log_level_set("*", STM_LOG_NONE);
+    stm_log_level_set("APP_MAIN", STM_LOG_INFO);
+
+    /* Create task */
+    xTaskCreate(example_task, "example_task", TASK_SIZE, NULL, TASK_PRIOR, NULL);
+
+    /* Start RTOS scheduler */
+    vTaskStartScheduler();
+}
---
+++ stepmotor.c
@@ -0,0 +1,188 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+
+#include "stm_log.h"
+#include "stepmotor.h"
+
+#define STEPMOTOR_INIT_ERR_STR                  "step motor init error"
+#define STEPMOTOR_SET_DIR_ERR_STR               "step motor set direction error"
+#define STEPMOTOR_TOGGLE_DIR_ERR_STR            "step motor toggle direction error"
+#define STEPMOTOR_SET_PWM_FREQ_ERR_STR          "step motor set pwm frequency error"
+#define STEPMOTOR_SET_PWM_DUTYCYCLE_ERR_STR     "step motor set pwm duty cycle error"
+#define STEPMOTOR_START_ERR_STR                 "step motor start error"
+#define STEPMOTOR_STOP_ERR_STR                  "step motor stop error"
+
+#define mutex_lock(x)                           while (xSemaphoreTake(x, portMAX_DELAY) != pdPASS)
+#define mutex_unlock(x)                         xSemaphoreGive(x)
+#define mutex_create()                          xSemaphoreCreateMutex()
+#define mutex_destroy(x)                        vQueueDelete(x)
+
+static const char* STEPMOTOR_TAG = "STEP MOTOR";
+#define STEPMOTOR_CHECK(a, str, ret)  if(!(a)) {                                            \
+        STM_LOGE(STEPMOTOR_TAG,"%s:%d (%s):%s", __FILE__, __LINE__, __FUNCTION__, str);     \
+        return (ret);                                                                       \
+        }
+
+typedef struct stepmotor {
+    gpio_port_t         dir_gpio_port;          /*!< Pin dir GPIO Port */
+    gpio_num_t          dir_gpio_num;           /*!< Pin dir GPIO Num */
+    timer_num_t         pulse_timer_num;        /*!< Pin pulse Timer Num */
+    timer_pins_pack_t   pulse_timer_pins_pack;  /*!< Pin pulse Timer Pins Pack */
+    timer_chnl_t        pulse_timer_chnl;       /*!< Pin pulse Timer Channel */
+    bool                dir;                    /*!< Direction */
+    uint32_t            freq_hz;                /*!< PWM frequency in Hz */
+    uint8_t             duty;                   /*!< PWM duty cycle in % */
+    stepmotor_status_t  status;                 /*!< Step motor status */
+    SemaphoreHandle_t   lock;                   /*!< Step motor mutex */
+} stepmotor_t;
+
+stepmotor_handle_t stepmotor_config(stepmotor_config_t *config)
+{
+    /* Check input conditions */
+    STEPMOTOR_CHECK(config, STEPMOTOR_INIT_ERR_STR, NULL);
+
+    /* Allocate memory for handle structure */
+    stepmotor_handle_t handle = calloc(1, sizeof(stepmotor_t));
+    STEPMOTOR_CHECK(handle, STEPMOTOR_INIT_ERR_STR, NULL);
+
+    /* Configure pin direction */
+    gpio_cfg_t dir_cfg;
+    dir_cfg.gpio_port = config->dir_gpio_port;
+    dir_cfg.gpio_num = config->dir_gpio_num;
+    dir_cfg.mode = GPIO_OUTPUT_PP;
+    dir_cfg.reg_pull_mode = GPIO_REG_PULL_NONE;
+    STEPMOTOR_CHECK(!gpio_config(&dir_cfg), STEPMOTOR_INIT_ERR_STR, NULL);
+    STEPMOTOR_CHECK(!gpio_set_level(config->dir_gpio_port, config->dir_gpio_num, 0), STEPMOTOR_INIT_ERR_STR, NULL);
+
+    /* Configure pin pulse */
+    pwm_cfg_t pulse_cfg;
+    pulse_cfg.timer_num = config->pulse_timer_num;
+    pulse_cfg.timer_pins_pack = config->pulse_timer_pins_pack;
+    pulse_cfg.timer_chnl = config->pulse_timer_chnl;
+    STEPMOTOR_CHECK(!pwm_config(&pulse_cfg), STEPMOTOR_INIT_ERR_STR, NULL);
+    STEPMOTOR_CHECK(!pwm_set_params(config->pulse_timer_num, config->pulse_timer_chnl, 0, 0), STEPMOTOR_INIT_ERR_STR, NULL);
+
+    /* Update handle structure */
+    handle->dir_gpio_port = config->dir_gpio_port;
+    handle->dir_gpio_num = config->dir_gpio_num;
+    handle->pulse_timer_num = config->pulse_timer_num;
+    handle->pulse_timer_pins_pack = config->pulse_timer_pins_pack;
+    handle->pulse_timer_chnl = config->pulse_timer_chnl;
+    handle->dir = 0;
+    handle->freq_hz = 0;
+    handle->duty = 50;
+    handle->status = STEPMOTOR_READY;
+    handle->lock = mutex_create();
+
+    return handle;
+}
+
+stm_err_t stepmotor_set_dir(stepmotor_handle_t handle, bool dir)
+{
+    mutex_lock(handle->lock);
+
+    int ret;
+    ret = gpio_set_level(handle->dir_gpio_port, handle->dir_gpio_num, dir);
+    if (ret) {
+        STM_LOGE(STEPMOTOR_TAG, STEPMOTOR_SET_DIR_ERR_STR);
+        mutex_unlock(handle->lock);
+        return STM_FAIL;
+    }
+
+    handle->dir = dir;
+    mutex_unlock(handle->lock);
+
+    return STM_OK;
+}
+
+stm_err_t stepmotor_toggle_dir(stepmotor_handle_t handle)
+{
+    mutex_lock(handle->lock);
+
+    int ret;
+    ret = gpio_toggle_level(handle->dir_gpio_port, handle->dir_gpio_num);
+    if (ret) {
+        STM_LOGE(STEPMOTOR_TAG, STEPMOTOR_TOGGLE_DIR_ERR_STR);
+        mutex_unlock(handle->lock);
+        return STM_FAIL;
+    }
+
+    handle->dir = !handle->dir;
+    mutex_unlock(handle->lock);
+
+    return STM_OK;
+}
+
+stm_err_t stepmotor_set_pwm_freq(stepmotor_handle_t handle, uint32_t freq_hz)
+{
+    mutex_lock(handle->lock);
+
+    int ret;
+    ret = pwm_set_params(handle->pulse_timer_num, handle->pulse_timer_chnl, handle->freq_hz, handle->duty);
+    if (ret) {
+        STM_LOGE(STEPMOTOR_TAG, STEPMOTOR_SET_PWM_FREQ_ERR_STR);
+        mutex_unlock(handle->lock);
+        return STM_FAIL;
+    }
+
+    handle->freq_hz = freq_hz;
+    mutex_unlock(handle->lock);
+
+    return STM_OK;
+}
+
+stm_err_t stepmotor_set_pwm_duty(stepmotor_handle_t handle, uint8_t duty)
+{
+    mutex_lock(handle->lock);
+
+    int ret;
+    ret = pwm_set_duty(handle->pulse_timer_num, handle->pulse_timer_chnl, duty);
+    if (ret) {
+        STM_LOGE(STEPMOTOR_TAG, STEPMOTOR_SET_PWM_DUTYCYCLE_ERR_STR);
+        mutex_unlock(handle->lock);
+        return STM_FAIL;
+    }
+
+    handle->duty = duty;
+    mutex_unlock(handle->lock);
+
+    return STM_OK;
+}
+
+stm_err_t stepmotor_start(stepmotor_handle_t handle)
+{
+    mutex_lock(handle->lock);
+
+    int ret;
+    ret = pwm_start(handle->pulse_timer_num, handle->pulse_timer_chnl);
+    if (ret) {
+        STM_LOGE(STEPMOTOR_TAG, STEPMOTOR_START_ERR_STR);
+        mutex_unlock(handle->lock);
+        return STM_FAIL;
+    }
+
+    handle->status = STEPMOTOR_RUNNING;
+    mutex_unlock(handle->lock);
+
+    return STM_OK;
+}
+
+stm_err_t stepmotor_stop(stepmotor_handle_t handle)
+{
+    mutex_lock(handle->lock);
+
+    int ret;
+    ret = pwm_stop(handle->pulse_timer_num, handle->pulse_timer_chnl);
+    if (ret) {
+        STM_LOGE(STEPMOTOR_TAG, STEPMOTOR_STOP_ERR_STR);
+        mutex_unlock(handle->lock);
+        return STM_FAIL;
+    }
+
+    handle->status = STEPMOTOR_STOP;
+    mutex_unlock(handle->lock);
+
+    return STM_OK;
+}
